bc3bc05059c202a468d5510a275a8ad2fd2517ce
Mahadi hassan <mahadi.hassan@northsouth.edu>
2019-07-28 09:50:10 +0600

    class based state replaced by global redux state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //   state = {
+  //     age: 20
+  //   };
+
+  //   onAgeUp = () => {
+  //     this.setState({
+  //       ...this.state,
+  //       age: this.state.age + 1
+  //     });
+  //   };
+
+  //   onAgeDown = () => {
+  //     this.setState({
+  //       ...this.state,
+  //       age: this.state.age - 1
+  //     });
+  //   };
+
+  render() {
+    return (
+      <div>
+        <div>
+          Age : <span>{this.props.age}</span>
+        </div>
+        <button onClick={this.props.onAgeUp}>Age Up</button>
+        <button onClick={this.props.onAgeDown}>Age Up</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    age: state.age
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAgeUp: () => dispatch({ type: "AGE_UP" }),
+    onAgeDown: () => dispatch({ type: "AGE_DOWN" })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
---
+++ src/store/reducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  age: 20
+};
+
+const reducer = (state = initialState, action) => {
+  const newState = { ...state };
+  if (action.type === "AGE_UP") {
+    newState.age++;
+  }
+
+  if (action.type === "AGE_DOWN") {
+    newState.age--;
+  }
+  return newState;
+};
+
+export default reducer;
